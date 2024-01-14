10a9cf57177c0289fc429e7ee58a0de93f3d9d5e
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-01-14 13:27:06 -0700

    Array placeholder and iteration through our object

--- script.js
+++ script.js
@@ -8,5 +8,26 @@ const inspiration = {
     date: Date(),
     specialMessage: ["Do what you can, with what you have, where you are", "A person who never made a mistake never tried anything new", "Never regret anything that made you smile", "You must do the thing you think you cannot do"],
     advice: ["Follow your passion", "Never stop learning.", "Focus on what you can control.", "There's more than one way of doing something."]
+};
+
+// Array to store your inspiration 
+let yourInspo = []
+
+// Object Iteration
+for (let inspo in inspiration) {
+    index = randomNumber(inspiration[prop].length)
+
+    switch(inspo) {
+        case 'date':
+            yourInspo.push(`Todays date is: ${inspiration(date)}`);
+        case 'specialMessage':
+            yourInspo.push(`Your special message is: ${specialMessage[inspo][index]}`);
+        case 'advice':
+            yourInspo.push(`Here is some adive, you got this: ${specialMessage[inspo[index]]}`)
+        default:
+            console.log('Today is just not your day, but that is okay. There is always tomorrow');
+    }
 }
-console.log(inspiration);
+
+
+//console.log(inspiration);
